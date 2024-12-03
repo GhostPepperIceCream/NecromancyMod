d4de9253614f846cd2894ad27492884fb430e330
GhostPepperIceCream <ghostpeppericecream@proton.me>
2024-12-02 20:23:59 -0600

    add new animation option

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,16 +1,16 @@
-﻿using StardewModdingAPI;
+using StardewModdingAPI;
 namespace NecromancyMod
 {
     public class ModConfig
     {
         public SButton summonItemFriendKey = SButton.J;
         public SButton controlMonstersKey = SButton.K;
-        public string summonAnimation = "Mini Harp";
+        public string summonAnimation = "Hand raised";
         public string itemSummon = "Ghost";
         public bool attackSlimesInHutch = false;
         public int baseDamage = 10;
 
-        internal static string[] summonAnimationChoices = new string[] { "Mini Harp", "Flute" };
+        internal static string[] summonAnimationChoices = new string[] { "Hand raised", "Mini Harp", "Flute" };
         internal static string[] itemSummonChoices = new string[] { "Ghost", "Skeleton", "Haunted Skull", "Mummy" };
     }
 }
