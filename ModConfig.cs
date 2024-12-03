3d1db1464782f7b3b3081d3dc6673904896070a1
GhostPepperIceCream <ghostpeppericecream@proton.me>
2024-12-03 09:39:26 -0600

    comment out unused options

--- ModConfig.cs
+++ ModConfig.cs
@@ -3,14 +3,14 @@ namespace NecromancyMod
 {
     public class ModConfig
     {
-        public SButton summonItemFriendKey = SButton.J;
+        //public SButton summonItemFriendKey = SButton.J;
         public SButton controlMonstersKey = SButton.K;
         public string summonAnimation = "Hand raised";
-        public string itemSummon = "Ghost";
+        //public string itemSummon = "Ghost";
         public bool attackSlimesInHutch = false;
         public int baseDamage = 10;
 
         internal static string[] summonAnimationChoices = new string[] { "Hand raised", "Mini Harp", "Flute" };
-        internal static string[] itemSummonChoices = new string[] { "Ghost", "Skeleton", "Haunted Skull", "Mummy" };
+        //internal static string[] itemSummonChoices = new string[] { "Ghost", "Skeleton", "Haunted Skull", "Mummy" };
     }
 }
